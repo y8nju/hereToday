cef16e64697faad33ff89a3d3a115ef12d95e0a6
y8nju <89aruc@gmail.com>
2022-09-26 01:55:30 +0900

    Add: 유저 설정

--- App.js
+++ App.js
@@ -17,6 +17,10 @@ import TalkScreen from './screens/talk/talkScreen';
 import talkWriteScreen from './screens/talk/talkWriteScreen';
 import TalkViewScreen from './screens/talk/talkViewScreen';
 import TalkUpdateScreen from './screens/talk/talkUpdateScreen';
+import UserSettingScreen from './screens/userInfo/userSettingScreen';
+import PassChangeScreen from './screens/account/passChangeScreen';
+import WithdrawScreen from './screens/account/withdrawScreen';
+import WithdrawSuccessScreen from './screens/account/withdrawSuccessScreen';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -27,11 +31,15 @@ function GuestStackNavigator() {
 		}}>
 		<Stack.Screen name="Login" component={LoginScreen} />
 		<Stack.Screen name="Register" component={RegisterScreen} />
+		<Stack.Screen name="WithdrawSuccess" component={WithdrawSuccessScreen} options={{title: '탈퇴 완료'}}/>
 	</Stack.Navigator>)
 }
 function MemberStackNavigator() {
 	return (<Stack.Navigator screenOptions={{animation: 'slide_from_right'}}>
 		<Stack.Screen name="Info" component={InfoScreen} options={{title: '프로필'}}/>
+		<Stack.Screen name="UserSetting" component={UserSettingScreen} options={{title: '계정 정보'}}/>
+		<Stack.Screen name="PassChange" component={PassChangeScreen} options={{title: '비밀번호 변경'}}/>
+		<Stack.Screen name="Withdraw" component={WithdrawScreen} options={{title: '서비스 탈퇴'}}/>
 	</Stack.Navigator>)
 }
 function TalkStackNavigator() {
@@ -39,7 +47,7 @@ function TalkStackNavigator() {
 		<Stack.Screen name="Talk" component={TalkScreen} options={{title: '이야기'}}/>
 		<Stack.Screen name="TalkWrite" component={talkWriteScreen} options={{title: '이야기 쓰기', presentation: 'modal', animation: 'fade_from_bottom'}}/>
 		<Stack.Screen name="TalkView" component={TalkViewScreen} options={{title: '이야기'}}/>
-		<Stack.Screen name="TalkUpdate" component={TalkUpdateScreen} options={{title: '이야기 수정하기', presentation: 'modal', animation: 'fade_from_bottom'}}/>
+		<Stack.Screen name="TalkUpdate" component={TalkUpdateScreen} options={{title: '이야기 수정하기'}}/>
 	</Stack.Navigator>)
 }
 function AccountStackNavigator() {
@@ -78,7 +86,7 @@ export default function App() {
 								<Ionicons name={focused ? 'newspaper-sharp' : 'newspaper-outline' } color={color} size={24} />
 							)}}/>
 						<Tab.Screen name="Account" component={AccountStackNavigator} 
-							options={{headerShown: false,
+							options={{headerShown: false, unmountOnBlur: true,
 								tabBarIcon: ({focused, color})=> (
 								<Ionicons name={focused ? 'md-person-circle' : 'md-person-circle-outline'} color={color} size={26}  />
 							)}} />
